af8e146dc9ae5cc3cfbdf19d2fb60ad7a63e2127
seattleowl <32573897+seattleowl@users.noreply.github.com>
2021-12-23 15:34:22 -0800

    wait for heroku to be slow

--- src/main.js
+++ src/main.js
@@ -7,28 +7,28 @@ const SERVER =
 		? "https://super-snow-shooter.herokuapp.com"
 		: "http://localhost:3000";
 
-const game = new Phaser.Game({
-	type: Phaser.AUTO,
-	scale: {
-		mode: Phaser.Scale.FIT,
-		width: 960,
-		height: 528,
-		autoCenter: true
-	},
-	physics: {
-		default: "arcade",
-		arcade: {
-			gravity: { y: 400 }
-		}
-	},
-	render: {
-		pixelArt: true
-	},
-	antialias: false,
-	loader: {
-		baseURL: "assets"
-	},
-	scene: GameScene
+connect(SERVER).then(() => {
+	const game = new Phaser.Game({
+		type: Phaser.AUTO,
+		scale: {
+			mode: Phaser.Scale.FIT,
+			width: 960,
+			height: 528,
+			autoCenter: true
+		},
+		physics: {
+			default: "arcade",
+			arcade: {
+				gravity: { y: 400 }
+			}
+		},
+		render: {
+			pixelArt: true
+		},
+		antialias: false,
+		loader: {
+			baseURL: "assets"
+		},
+		scene: GameScene
+	});
 });
-
-connect(SERVER);
