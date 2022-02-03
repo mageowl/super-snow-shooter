a644a38c7aaf822fb7dc87c4f4eeabb78781b69f
seattleowl <32573897+seattleowl@users.noreply.github.com>
2022-02-02 16:14:39 -0800

    i change my domain too much

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const httpServer = createServer(app);
 /** @type {Server} */
 const io = new Server(httpServer, {
 	cors: {
-		origin: debug ? "http://localhost:5500" : "https://mageowls.com",
+		origin: debug ? "http://localhost:5500" : "https://mageowlstudios.com",
 		methods: ["GET", "POST"]
 	}
 });
