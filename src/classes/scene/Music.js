46c7bb8c17b462099bf867bbd0873c3abf345e5d
seattleowl <32573897+seattleowl@users.noreply.github.com>
2022-02-16 18:33:17 -0800

    m to pause

--- src/classes/scene/Music.js
+++ src/classes/scene/Music.js
@@ -22,14 +22,23 @@ export default class Music extends UpdatedScene {
 				disc.setTexture("disc.bananax");
 			})
 			.on("pointerdown", () => {
-				this.pause();
+				// this.next();
 			});
 		this.add.bitmapText(64, 480, "zepto-name-tag", "Music by Bananax", 24);
+		this.add.bitmapText(64, 504, "zepto-small", "M to pause", 16);
+
+		this.input.keyboard.on("keydown-M", () => this.pause());
 	}
 
 	pause() {
 		this.paused = !this.paused;
 		if (this.paused) this.tracks[this.currentlyPlaying].pause();
 		else this.tracks[this.currentlyPlaying].resume();
+	}
+
+	next() {
+		this.tracks[this.currentlyPlaying].stop();
+		this.currentlyPlaying = (this.currentlyPlaying + 1) % this.tracks.length;
+		this.tracks[this.currentlyPlaying].play({ loop: true, volume: 0.5 });
 	}
 }
